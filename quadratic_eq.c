0614fd7e4a4d5aa939b6bcc8ea209bbbd7c6c618
morhuhan <radionovich.arkadiy@mail.ru>
2021-10-18 21:10:55 +0500

    first commit

--- quadratic_eq.c
+++ quadratic_eq.c
@@ -1,44 +1,43 @@
 #include <stdio.h>
 #include <math.h>
 
-double fquadr(double a, double b, double c, double *x1, double *x2)
-{
-    double D, xa, xb;
-
-    D = a;
-
-    xa = 1; 
-    xb = 2;
-
-    printf("%f \n", D);
-    printf("%f \n", xa);
-    printf("%f \n", xb);
-
+float fquadr(float a, float b, float c, float *x1, float *x2) {
+    float D, xa, xb;
+    int flag = 1;
+        D = (b * b) - (4 * a * c);
+        if (D < 0) {
+            flag = 0;
+        } 
+        else if (D == 0) {
+            printf("Discriminant = 0, => x1 = x2 \n");
+            xa = ((-b) - sqrt(D)) / 2;
+            xa = xb;
+        }
+        else {
+            xa = ((-1*b) - sqrt(D)) / 2; 
+            xb = ((-1*b) + sqrt(D)) / 2;
+        }
     *x1 = xa; 
-
     *x2 = xb;
+    return flag;
 }
 
-
-
-
-void main (void)
-{
-    double a,b,c,x1,x2;
-
-    printf ("Hello...");
+void main (void) {
+    float a,b,c,x1,x2;
+    int flag = 1;
+    printf ("Hello. This program will help you calculate the roots of a quadratic equation");
     scanf ("d");
-
     printf ("Input a");
     scanf ("%f", &a);
-
     printf ("Input b");
     scanf ("%f", &b);
-
     printf ("Input c");
     scanf ("%f", &c);
-
-    fquadr(a,b,c, &x1, &x2);
-
-    printf ("%f \n %f \n", x1, x2);
+    flag = fquadr(a,b,c, &x1, &x2);
+    if (flag == 0) {
+        printf("Discriminant < 0, => x1, x2 not exists \n");
+    }
+    else {
+        printf ("x1 = %f \n x2 = %f \n", x1, x2);
+    }
 }
