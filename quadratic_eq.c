626b5fd8ee868bba555e50d3150460e793364cc8
morhuhan <radionovich.arkadiy@mail.ru>
2021-10-19 21:37:01 +0500

    finally commit

--- quadratic_eq.c
+++ quadratic_eq.c
@@ -3,10 +3,10 @@
 
 float fquadr(float a, float b, float c, float *x1, float *x2) {
     float D, xa, xb;
-    int flag = 1;
         D = (b * b) - (4 * a * c);
         if (D < 0) {
-            flag = 0;
+            printf("Discriminant < 0, => x1, x2 not exists \n");
+            return 0;
         } 
         else if (D == 0) {
             printf("Discriminant = 0, => x1 = x2 \n");
@@ -19,25 +19,22 @@ float fquadr(float a, float b, float c, float *x1, float *x2) {
         }
     *x1 = xa; 
     *x2 = xb;
-    return flag;
+    return 1;
 }
 
 void main (void) {
     float a,b,c,x1,x2;
-    int flag = 1;
-    printf ("Hello. This program will help you calculate the roots of a quadratic equation");
+    int flag;
+    printf ("Hello. This program will help you calculate the roots of a quadratic equation (press ENTER)");
     scanf ("d");
-    printf ("Input a");
+    printf ("Input a: ");
     scanf ("%f", &a);
-    printf ("Input b");
+    printf ("Input b: ");
     scanf ("%f", &b);
-    printf ("Input c");
+    printf ("Input c: ");
     scanf ("%f", &c);
     flag = fquadr(a,b,c, &x1, &x2);
-    if (flag == 0) {
-        printf("Discriminant < 0, => x1, x2 not exists \n");
-    }
-    else {
+    if (flag != 0) {
         printf ("x1 = %f \n x2 = %f \n", x1, x2);
     }
 }
